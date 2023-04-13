6cc6560ee9a28e581160729add2f8c3ee535217f
sambonuruddeen <sambonuruddeen@gmail.com>
2023-04-13 15:41:36 +0100

    State & LGA Migrations

---
+++ app/Models/lga.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class lga extends Model
+{
+    use HasFactory;
+
+    public $table="lgas";
+    
+    public $fillable = [
+        'state_id',
+        'name',
+    ];
+}
