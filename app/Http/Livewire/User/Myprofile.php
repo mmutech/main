aee23dd8b9709114af6809369aee2353c291a8ca
engr-iliyas <77857252+engr-iliyas@users.noreply.github.com>
2023-05-02 13:27:20 +0100

    Started

--- app/Http/Livewire/User/Myprofile.php
+++ app/Http/Livewire/User/Myprofile.php
@@ -4,10 +4,22 @@ namespace App\Http\Livewire\User;
 
 use Livewire\Component;
 
+use App\Models\Biodata;
+
 class Myprofile extends Component
 {
+    public $employee;
+
+    public function mount()
+    {
+        $user = auth()->user();
+        $this->employee = Biodata::find(1);
+        // $this->employee = Biodata::find($user->biodata_id);
+    }
     public function render()
     {
+        $user = auth()->user();
+
         return view('livewire.user.myprofile')->layout('layouts.user-layout');
     }
 }
