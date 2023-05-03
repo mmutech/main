cddc464f7d1e66c637b3ab74de67ed82988edfa2
engr-iliyas <77857252+engr-iliyas@users.noreply.github.com>
2023-05-03 13:30:40 +0100

    Implementation of user profile with store data

--- app/Http/Livewire/User/Myprofile.php
+++ app/Http/Livewire/User/Myprofile.php
@@ -5,21 +5,45 @@ namespace App\Http\Livewire\User;
 use Livewire\Component;
 
 use App\Models\Biodata;
+use App\Models\MedicalHistory;
+use App\Models\NextOfKin;
+use App\Models\Bank;
+use App\Models\Deployment;
+use App\Models\DivisionModel;
+use App\Models\DepartmentModel;
+use App\Models\UnitModel;
+use App\Models\LocationModel;
+use App\Models\AreaOffice;
+use App\Models\Feeder;
+use App\Models\Education;
+use App\Models\Qualification;
+
 
 class Myprofile extends Component
 {
-    public $employee;
+    public $user, $employee, $medical, $kin, $bank, $deployment, $division, $department, $unit, $location, $ao, $feeder, $employee_id;
 
     public function mount()
     {
-        $user = auth()->user();
-        $this->employee = Biodata::find(1);
-        // $this->employee = Biodata::find($user->biodata_id);
+        $this->user = auth()->user();
+        // $this->employee_id = $this->user->id;
+        $this->employee_id = 1;
+
+        $this->employee = Biodata::find($this->employee_id);
+        $this->medical = MedicalHistory::find($this->employee_id);
+        $this->kin = NextOfKin::find($this->employee_id);
+        $this->bank = Bank::find($this->employee_id);
+        $this->deployment = Deployment::find($this->employee_id);
+
+        $this->division = DivisionModel::find($this->deployment->division_id);
+        $this->department = DepartmentModel::find($this->deployment->department_id);
+        $this->unit = UnitModel::find($this->deployment->unit_id);
+        $this->location = LocationModel::find($this->deployment->location_id);
+        $this->ao = AreaOffice::find($this->deployment->area_office_id);
+        $this->feeder = Feeder::find($this->deployment->feeder_id);
     }
     public function render()
     {
-        $user = auth()->user();
-
         return view('livewire.user.myprofile')->layout('layouts.user-layout');
     }
 }
